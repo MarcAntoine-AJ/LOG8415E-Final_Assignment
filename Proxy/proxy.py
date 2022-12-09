bc2f0d75dd27a990af50a7f124dedf25b9beec1b
Marc-Antoine Abou Jaoude <marc.abou.jaoude@hotmail.com>
2022-12-08 20:51:15 -0500

    third try proxy

--- Proxy/proxy.py
+++ Proxy/proxy.py
@@ -7,10 +7,10 @@ from sshtunnel import SSHTunnelForwarder
 slaves = []
 
 def connect(slave_ip, master_ip):
-    #with SSHTunnelForwarder (slave_ip, ssh_username='ubuntu', ssh_pkey='keypair.pem', remote_bind_address=(master_ip, 3306)) as tunnel:
-    conn = pymysql.connect(host=master_ip, user='user', password='password', db='sakila', port=3306, autocommit=True)
-    print(conn)
-    return conn
+    with SSHTunnelForwarder (slave_ip, ssh_username='ubuntu', ssh_pkey='keypair.pem', remote_bind_address=(master_ip, 3306)) as tunnel:
+        conn = pymysql.connect(host=master_ip, user='user', password='password', db='sakila', port=3306, autocommit=True)
+        print(conn)
+        return conn
 
 def ping_host(host):
     return ping(target=host, count=1, timeout=2).rtt_avg_ms
@@ -20,5 +20,5 @@ def lowest_res_slave():
     fastest_slave = min(range(len(response_times)), key=response_times.__getitem__)
     return fastest_slave
     
-connection = connect('54.158.41.150', '34.204.84.12')
+connection = connect('34.204.84.12', '54.158.41.150')
 
